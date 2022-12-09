6dd5291b3a3dc757cb2a1e930a4438cfffa44e51
John Wohlfert <jwohlfert@nexient.com>
2022-12-09 14:00:47 -0500

    created snake class to encapsulate snake behavior

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
 from turtle import Screen, Turtle
 import time
 
+from snake import Snake
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-snake_list = []
-for i in range(3):
-    turtle = Turtle()
-    turtle.color("white")
-    turtle.shape("square")
-    turtle.penup()
-    turtle.setx(i * -20)
-    snake_list.append(turtle)
-screen.update()
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(snake_list) - 1, 0, -1):
-        new_x = snake_list[seg_num - 1].xcor()
-        new_y = snake_list[seg_num - 1].ycor()
-        snake_list[seg_num].goto(x=new_x, y=new_y)
-    snake_list[0].forward(20)
-
-
+    snake.move()
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.num_starting_segments = 3
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in range(self.num_starting_segments):
+            turtle = Turtle()
+            turtle.color("white")
+            turtle.shape("square")
+            turtle.penup()
+            turtle.setx(i * -20)
+            self.segments.append(turtle)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x=new_x, y=new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
